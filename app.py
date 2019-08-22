d323d7f90b0fae585f57d5d8ef64dfcd9b66b3f6
roger-selzler <roger.selzler@carleton.ca>
2019-08-22 12:53:13 -0400

    New edits.

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'Hello World'
+   return 'Hello World from /'
+
+@app.route('/home')
+def home():
+    return render_template("index.html")
+#app.add_url_rule('/home','home',render_template("index.html"))
+
+@app.route('/registerActivity')
+def registerActivity():
+    return render_template("registerActivity.html")
 
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
--- init.sh
+++ init.sh
@@ -7,3 +7,33 @@ virtualenv /venv
 source /venv/bin/activate
 pip install flask
 
+
+
+# -- Install the mongoDB package
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add - 
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+
+sudo apt-get update
+sudo apt-get install -y mongodb-org 
+
+
+
+# -- add autocompletion functionalities for vim
+cd ~/ 
+sudo apt-get install curl vim exuberant-ctags git ack-grep
+sudo pip install pep8 flake8 pyflakes isort yapf 
+wget https://raw.github.com/fisadev/fisa-vim-config/master/.vimrc -O ~/.vimrc
+vim -c :q! ~/.vimrc
+
+# Create autocompletion for python on bash shell
+[ -f ~/.pythonrc ] && echo "File ~/.pythonrc already exist" || touch ~/.pythonrc && echo "try:
+    import readline
+    import rlcompleter
+    readline.parse_and_bind(\"tab: complete\")
+except ImportError:
+    print(\"Module readline not available.\")" > ~/.pythonrc
+
+[ -f ~/.bashrc ] || touch ~/.bashrc 
+grep -qxF 'export PYTHONSTARTUP=~/.pythonrc' ~/.bashrc && echo 'export PYTHONSTARTUP=~/.pythonrc already exists on ~/.bashrc' || echo 'export PYTHONSTARTUP=~/.pythonrc' >> ~/.bashrc
+
+
